d0fc1095198b3535520801c4a4fcbb3724283b7c
Sam Hildebrand <sch_numberone@yahoo.com>
2021-10-23 16:18:19 -0500

    Remove a testing gate

--- gates/Nand2.py
+++
@@ -1,7 +0,0 @@
-# input1 input2
-def function (inputs):
-    input1 = inputs[0]
-    input2 = inputs[1]
-    import gates.NandGate
-    nand1 = gates.NandGate.function([input1, input2])
-    return nand1
